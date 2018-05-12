a98a17288e0924088be34d6b1d827ef0e99f6c5b
John Yatco <yatcoj@wit.edu>
2018-05-12 14:32:24 -0400

    side?

--- js/init.js
+++ js/init.js
@@ -3,8 +3,7 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-    $(document).ready(function(){
-       $('.scrollspy').scrollSpy();
-     });
+    $('.scrollspy').scrollSpy();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
